83fe7a41e1a6a81a21ef1dde7949f6c075eac283
Artiom Kenibasov <artiom@gmail.com>
2024-12-22 04:08:28 +0100

    initialize Next.js project with authentication and dashboard layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { AppProvider } from '@toolpad/core/nextjs';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import type { Navigation } from '@toolpad/core/AppProvider';
+import { SessionProvider, signIn, signOut } from 'next-auth/react';
+import { auth } from '../auth';
+
+const NAVIGATION: Navigation = [
+    {
+        kind: 'header',
+        title: 'Meniu',
+    },
+    {
+        title: 'Home',
+        icon: <DashboardIcon />,
+    },
+    {
+        segment: 'orders',
+        title: 'Orders',
+        icon: <ShoppingCartIcon />,
+    },
+];
+
+const BRANDING = {
+    title: 'NSRomania',
+
+};
+
+const AUTHENTICATION = {
+    signIn,
+    signOut,
+};
+
+export default async function RootLayout(props: { children: React.ReactNode }) {
+    const session = await auth();
+
+    return (
+        <html lang="en" data-toolpad-color-scheme="light">
+            <body>
+                <SessionProvider session={session}>
+                    <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+                        <AppProvider
+                            navigation={NAVIGATION}
+                            branding={BRANDING}
+                            session={session}
+                            authentication={AUTHENTICATION}
+                        >
+                            {props.children}
+                        </AppProvider>
+                    </AppRouterCacheProvider>
+                </SessionProvider>
+            </body>
+        </html>
+    );
+}

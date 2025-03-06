82a40e7bddd8d47572ac81c39f04f338f9d823b2
Artiom Kenibasov <artiom@gmail.com>
2025-03-06 21:09:10 +0100

    Refactor GET request handler to improve parameter handling and URL construction

--- src/app/api/domains/by-subdomain/[subdomain]/route.ts
+++ src/app/api/domains/by-subdomain/[subdomain]/route.ts
@@ -1,16 +1,25 @@
+// /src/app/api/domains/by-subdomain/[subdomain]/route.ts
 import { getNSDomainBySubdomain } from '@/lib/services/domains';
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-export async function GET(req: NextRequest, { params }: { params: { subdomain: string } }) {
-    const subdomain = await params.subdomain;
+type Props = {
+    params: Promise<{
+        subdomain: string
+    }>
+}
+
+export async function GET(request: Request, props: Props) {
+    const params = await props.params
+    const { subdomain } = await params;
+
     const domain = await getNSDomainBySubdomain(subdomain);
 
     if (!domain) {
         return NextResponse.json({ error: "Not found" }, { status: 404 });
     }
 
-    const absoluteUrl = new URL(`/api/domains/${domain.id}`, req.nextUrl.origin);
+    const currentUrl = new URL(request.url);
+    const absoluteUrl = new URL(`/api/domains/${domain.id}`, currentUrl.origin);
 
     return NextResponse.redirect(absoluteUrl.toString());
 }

2b40502c2bd3122ae09adbdc23daf0a9c5c6258b
AndreyVLD <nicula2003@gmail.com>
2025-07-31 02:58:20 +0200

    Added translations to all errors and now all the endpoints (except the register endpoint) has proper schema validation and translations for errors. Errors from the endpoints are displayed in the frontend through Snack or field errors

--- src/app/api/register/validate-subdomain/route.ts
+++ src/app/api/register/validate-subdomain/route.ts
@@ -2,11 +2,26 @@ import { NextRequest } from 'next/server';
 import { validateSubdomain } from '@/lib/services/registration';
 import { validateCaptcha } from '@/lib/services/recaptcha';
 import { validateSubdomainRequestSchema } from '@/lib/validations';
+import { hasLocale } from 'next-intl';
+import { locales } from '@/i18n/config';
+import { getTranslations } from 'next-intl/server';
 
 export async function POST(req: NextRequest) {
+    // Setting up the translation
+    const locale = req.headers.get('accept-language') ?? '';
+
+    if (!hasLocale(locales, locale)) {
+        return new Response(JSON.stringify({ error: 'Invalid locale' }), {
+            status: 400,
+        });
+    }
+
+    const t = await getTranslations({ locale, namespace: 'RegisterPage' });
+
+    // Parsing the request body
     const body = await req.json();
 
-    const parsed = validateSubdomainRequestSchema.safeParse(body);
+    const parsed = validateSubdomainRequestSchema(t).safeParse(body);
 
     if (!parsed.success) {
         // Return the first validation error message
@@ -18,13 +33,13 @@ export async function POST(req: NextRequest) {
     const { token, subdomain } = parsed.data;
 
     if (!(await validateCaptcha(token))) {
-        return new Response(JSON.stringify({ error: 'Failed to verify captcha' }), {
+        return new Response(JSON.stringify({ error: t('errors.reCAPTCHA.failed') }), {
             status: 405,
         });
     }
 
     if (!(await validateSubdomain(subdomain))) {
-        return new Response(JSON.stringify({ error: 'Subdomain is already taken' }), {
+        return new Response(JSON.stringify({ error: t('errors.subdomain.taken') }), {
             status: 409,
             headers: { 'Content-Type': 'application/json' },
         });
